ef1336eb876d73b72608b9d99724dc138485fbf2
Alex Gil <colibri.alex@gmail.com>
2020-09-15 19:32:14 -0400

    modified appender

--- appender.py
+++ appender.py
@@ -1,12 +1,12 @@
 import os
 import re
 
-chiens = open("chiens.txt","r")
-t = chiens.read()
-s = re.compile("\[.+blank'}").split(t)
+cahier = open("cahier.txt","r")
+t = cahier.read()
+s = re.compile("-- [1-9][1-9] (.*)--").split(t)
 fl = []
 
-for filename in os.listdir("_chiens/"):
+for filename in os.listdir("_cahier/"):
 	fl.append(filename)
 
 fl.sort()
@@ -14,7 +14,7 @@ fl.sort()
 n = 0
 
 for segment in s:
-	nf = open("_chiens/" + fl[n], "a")
+	nf = open("_cahier/" + fl[n], "a")
 	nf.write(segment)
 	nf.close()
 	n = n + 1
